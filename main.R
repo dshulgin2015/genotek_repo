cc9393debeb9f228f815c07c1d064550a31519a8
dshulgin2015 <unetby2016@yandex.ru>
2016-07-06 15:36:47 +0300

    random forest has been trained

--- main.R
+++ main.R
@@ -1,11 +1,24 @@
-setwd("/home/user/genotek/")
+setwd("/home/user/genotek_repo/")
 library(bigmemory)
 library(biganalytics)
 library(bigpca)
-w <- read.big.matrix(file.path("Learning_sample.txt"), type="integer", sep = " ")
-w <- big.t(w,verbose=TRUE)
+library(gbm)
+library(randomForest)
+train <- read.big.matrix(file.path("Learning_sample.txt"), type="integer", sep = " ")
+test <- read.big.matrix(file.path("Test_sample.txt"), type="integer", sep = " ")
 
-cl <- bigkmeans(w, centers = 2, iter.max = 10, nstart = 1, dist = "cosine")
+train <- big.t(train,verbose=TRUE)
+test <- big.t(test,verbose=TRUE)
+
+
+cl <- bigkmeans(train, centers = 2, iter.max = 10, nstart = 1, dist = "euclid")
+
+classes <- cl$cluster
+
+#colnames(train) <- paste("V",1:260121,sep = "")
+
+rs <- randomForest(x = train[,],y = classes)
+
 
 #library(FactoMineR)
 #cah.test <- HCPC(x, graph=FALSE, nb.clust=-1)
